1dc58c6b071a6a1b66b494b82e2048ddd404fe27
junland <unlandj2012@gmail.com>
2025-08-12 16:17:24 -0500

    refactor INSTALL.sh to use verbose and mode options for install commands

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,17 +7,19 @@ PORT_DIR=/usr/ports
 REVDEPD=/etc/revdep.d
 REVDEPCONF=/etc/revdep.conf
 
-install -d ${DESTDIR}${BINDIR}
-install -d ${DESTDIR}${CONFDIR}
-install -d ${DESTDIR}${PORT_DIR}
-install -d ${DESTDIR}${REVDEPD}
+install -dv ${DESTDIR}${BINDIR}
+install -dv ${DESTDIR}${CONFDIR}
+install -dv ${DESTDIR}${PORT_DIR}
+install -dv ${DESTDIR}${REVDEPD}
 
-install -dm777 ${DESTDIR}${CACHE_DIR}/packages
-install -dm777 ${DESTDIR}${CACHE_DIR}/sources
-install -dm777 ${DESTDIR}${CACHE_DIR}/work
+install -dvm777 ${DESTDIR}${CACHE_DIR}/packages
+install -dvm777 ${DESTDIR}${CACHE_DIR}/sources
+install -dvm777 ${DESTDIR}${CACHE_DIR}/work
 
-install -m755 xchroot revdep pkgadd pkgdel pkgbuild scratch updateconf portsync \
-              pkgbase pkgdepends pkgrebuild portcreate ${DESTDIR}${BINDIR}
-install -m644 scratchpkg.conf scratchpkg.repo scratchpkg.alias scratchpkg.mask ${DESTDIR}${CONFDIR}
+install -vm755 pkgadd pkgbase pkgbuild pkgdel pkgdepends pkgrebuild ${DESTDIR}${BINDIR}
 
-install -m644 revdep.conf ${DESTDIR}${REVDEPCONF}
+install -vm755 portcreate portsync revdep scratch updateconf xchroot ${DESTDIR}${BINDIR}
+
+install -vm644 scratchpkg.alias scratchpkg.conf scratchpkg.mask scratchpkg.repo ${DESTDIR}${CONFDIR}
+
+install -vm644 revdep.conf ${DESTDIR}${REVDEPCONF}
